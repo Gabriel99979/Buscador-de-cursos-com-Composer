4a1b95110c8a86bf7cca0ec24f38c7d924c3b6cf
Gabriel Amaral <gabriel99979@gmail.com>
2023-02-08 13:38:37 -0300

    Adicionando GuzzleHttp e DomCraler

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,6 +22,8 @@ class ComposerAutoloaderInitfa3a305d631eff240b769edee1a2ed13
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInitfa3a305d631eff240b769edee1a2ed13', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInitfa3a305d631eff240b769edee1a2ed13', 'loadClassLoader'));
@@ -30,6 +32,18 @@ class ComposerAutoloaderInitfa3a305d631eff240b769edee1a2ed13
         call_user_func(\Composer\Autoload\ComposerStaticInitfa3a305d631eff240b769edee1a2ed13::getInitializer($loader));
 
         $loader->register(true);
+
+        $filesToLoad = \Composer\Autoload\ComposerStaticInitfa3a305d631eff240b769edee1a2ed13::$files;
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
+            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+                require $file;
+            }
+        }, null, null);
+        foreach ($filesToLoad as $fileIdentifier => $file) {
+            $requireFile($fileIdentifier, $file);
+        }
 
         return $loader;
     }
